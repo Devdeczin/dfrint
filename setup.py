9ac38fe2cba46c48549cb13c9853d5e8de9dcdb4
Devdeczin <gabriellucasdevagnn@gmail.com>
2024-06-04 10:33:06 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="dfrint",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "rich",
+    ],
+    author="",
+    author_email="",
+    description="A package with delay_print, delay_input and fast_print",
+    url="https://github.com/Devdeczin/dfrint",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6 rich',
+)
